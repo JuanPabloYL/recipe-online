3e4b86a6eb008e61e10432f06d3f39eca1c7412d
Juan Pablo Yagüe <juanpabloyague@gmail.com>
2022-12-29 13:12:29 -0500

    add form for search recipe

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,6 +1,19 @@
-import { toggleBtnNav, navBarLinks } from "./nodes.js";
+import {
+  toggleBtnNav,
+  navBarLinks,
+  searchButton,
+  recipeForm,
+  closeFormButton,
+} from "./nodes.js";
 
-console.log(navBarLinks);
 toggleBtnNav.addEventListener("click", () => {
   navBarLinks.classList.toggle("active");
+});
+
+searchButton.addEventListener("click", () => {
+  recipeForm.classList.add("active");
+});
+
+closeFormButton.addEventListener("click", () => {
+  recipeForm.classList.remove("active");
 });
--- src/js/nodes.js
+++ src/js/nodes.js
@@ -1,2 +1,5 @@
 export const toggleBtnNav = document.querySelector(".recipe-header__toggle");
 export const navBarLinks = document.querySelector(".recipe-header__links");
+export const searchButton = document.querySelector("#search");
+export const recipeForm = document.querySelector(".recipe-form");
+export const closeFormButton = document.querySelector("#close");
